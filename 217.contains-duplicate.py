c3f0cea5f2c836f206ea297bba517987bbf503ac
TTC <42894515+fengsy97@users.noreply.github.com>
2023-02-21 18:04:40 +0800

    217 and 11

---
+++ 217.contains-duplicate.py
@@ -0,0 +1,52 @@
+#
+# @lc app=leetcode id=217 lang=python3
+#
+# [217] Contains Duplicate
+#
+# https://leetcode.com/problems/contains-duplicate/description/
+#
+# algorithms
+# Easy (61.42%)
+# Likes:    8339
+# Dislikes: 1102
+# Total Accepted:    2.5M
+# Total Submissions: 4.1M
+# Testcase Example:  '[1,2,3,1]'
+#
+# Given an integer array nums, return true if any value appears at least twice
+# in the array, and return false if every element is distinct.
+# 
+# 
+# Example 1:
+# Input: nums = [1,2,3,1]
+# Output: true
+# Example 2:
+# Input: nums = [1,2,3,4]
+# Output: false
+# Example 3:
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= nums.length <= 10^5
+# -10^9 <= nums[i] <= 10^9
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        judgeset = set()
+        for num in nums:
+            # print(judgeset)
+            if num in judgeset:
+                return True
+            else:
+                judgeset.add(num)
+        return False
+# @lc code=end
+
