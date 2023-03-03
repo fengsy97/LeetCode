ccec15ccbb3d51cd7502ce83fc4317146f635a32
TTC <42894515+fengsy97@users.noreply.github.com>
2023-03-04 00:37:15 +0800

    876

---
+++ 876.middle-of-the-linked-list.py
@@ -0,0 +1,68 @@
+#
+# @lc app=leetcode id=876 lang=python3
+#
+# [876] Middle of the Linked List
+#
+# https://leetcode.com/problems/middle-of-the-linked-list/description/
+#
+# algorithms
+# Easy (75.28%)
+# Likes:    8891
+# Dislikes: 259
+# Total Accepted:    1.2M
+# Total Submissions: 1.6M
+# Testcase Example:  '[1,2,3,4,5]'
+#
+# Given the head of a singly linked list, return the middle node of the linked
+# list.
+# 
+# If there are two middle nodes, return the second middle node.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: head = [1,2,3,4,5]
+# Output: [3,4,5]
+# Explanation: The middle node of the list is node 3.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: head = [1,2,3,4,5,6]
+# Output: [4,5,6]
+# Explanation: Since the list has two middle nodes with values 3 and 4, we
+# return the second one.
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of nodes in the list is in the range [1, 100].
+# 1 <= Node.val <= 100
+# 
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        first = head
+        if(not first): return first
+        second = first.next
+        if(not second): return first
+        while(first and second):
+            first=first.next
+            if(not second.next):
+               return first
+            second = second.next.next
+        return first
+# @lc code=end
+
